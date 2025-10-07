2c35584ccbcb4ea1f47a40a9b092df020eb85efd
Roman Marek~ <marekr@bacil.net>
2025-10-07 23:05:55 +0200

    Refactor ESLint configuration and update package dependencies
    - Changed ESLint configuration to target only JavaScript files in the `src` directory.
    - Removed obsolete test file configurations from ESLint settings.
    - Simplified `lint-staged` command in `package.json` by removing the explicit config reference.

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 module.exports = [
     {
-        files: ['**/*.js'],
+        files: ['src/*.js'],
         languageOptions: {
             ecmaVersion: 'latest',
             sourceType: 'commonjs',
@@ -25,45 +25,5 @@ module.exports = [
             indent: ['error', 4],
         },
     },
-    {
-        files: ['test/**/*.js'],
-        languageOptions: {
-            ecmaVersion: 'latest',
-            sourceType: 'commonjs',
-            globals: {
-                console: 'readonly',
-                process: 'readonly',
-                Buffer: 'readonly',
-                __dirname: 'readonly',
-                __filename: 'readonly',
-                module: 'readonly',
-                require: 'readonly',
-                exports: 'readonly',
-                global: 'readonly',
-                // Jest globals
-                describe: 'readonly',
-                it: 'readonly',
-                test: 'readonly',
-                expect: 'readonly',
-                beforeEach: 'readonly',
-                afterEach: 'readonly',
-                beforeAll: 'readonly',
-                afterAll: 'readonly',
-                jest: 'readonly',
-                // Node.js timer functions
-                setTimeout: 'readonly',
-                clearTimeout: 'readonly',
-                setInterval: 'readonly',
-                clearInterval: 'readonly',
-            },
-        },
-        rules: {
-            'no-console': 'off',
-            'no-unused-vars': 'warn',
-            'no-undef': 'error',
-            semi: ['error', 'always'],
-            quotes: ['error', 'single'],
-            indent: ['error', 4],
-        },
-    },
+    
 ];
